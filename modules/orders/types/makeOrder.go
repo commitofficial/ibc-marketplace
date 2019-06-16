80f79c12197ac702e316548a8eaa8fc1c1ce544f
commitofficial <commitofficial@protonmail.com>
2019-06-16 14:25:42 +0530

    initialized module structure

---
+++ modules/orders/types/makeOrder.go
@@ -0,0 +1,38 @@
+package types
+
+import (
+	ctypes "github.com/comdex-blockchain/types"
+)
+
+type OrderStatus byte
+
+const (
+	StatusUnFilled  OrderStatus = 0x01
+	StatusCancelled OrderStatus = 0x02
+	StatusFilled    OrderStatus = 0x03
+)
+
+func (status OrderStatus) String() string {
+	switch status {
+	case StatusUnFilled:
+		return "UnFilled"
+	case StatusCancelled:
+		return "Cancelled"
+	case StatusFilled:
+		return "Filled"
+	default:
+		return ""
+	}
+}
+
+// Base MakeOrder
+type BaseMakeOrder struct {
+	BaseToken        ctypes.Coin
+	QuoteToken       ctypes.Coin
+	MakerAddress     ctypes.AccAddress
+	TakerAddress     ctypes.AccAddress
+	ExpirationHeight int64
+	OrderHash        []byte
+	Signature        []byte
+	Status           OrderStatus
+}
